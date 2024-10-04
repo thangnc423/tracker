c1f5ee6ae417f6e704d030ea799f57d3502ade95
Thang Chau <thangnc@uci.edu>
2024-10-04 15:07:43 -0700

    finished nav bar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -4,17 +4,25 @@ import logo from '../images/logo.png'
 import React from 'react'
 
 const NavBar = () => {
-  return (
-    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
-        <Link href="/">
-            <Image src={logo} alt={"flamechasers"} width={50} height={50}/>
-        </Link>
-        <ul className='flex space-x-6'>
-            <li><Link href="/">Dashboard</Link></li>
-            <li><Link href="/reviews">Reviews</Link></li>
-        </ul>
-    </nav>
-  )
+    const links = [
+        {label: 'Dashboard', href: '/'},
+        {label: 'Reviews', href: '/reviews'},
+    ]
+
+    return (
+        <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
+            <Link href="/">
+                <Image src={logo} alt={"flamechasers"} width={50} height={50}/>
+            </Link>
+            <ul className='flex space-x-6'>
+                {links.map(link => 
+                <Link 
+                    key={link.href} 
+                    className='text-purple-500 hover:text-purple-800 transition-colors' 
+                    href={link.href}>{link.label}</Link>)}
+            </ul>
+        </nav>
+    )
 }
 
 export default NavBar
